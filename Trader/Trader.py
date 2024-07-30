09cf484e84a6df9ee811db9943711e8f535697c3
smarinacseas <smarinac@seas.upenn.edu>
2024-07-29 22:15:41 -0500

    Anaconda installed, virtual environment created/activated, imports

---
+++ Trader/Trader.py
@@ -0,0 +1,14 @@
+'''
+Activate environment: conda activate quant
+Deactivate: conda deactivate
+'''
+
+# Imports
+import pandas as pd
+import ccxt
+import numpy as np
+import datetime as dt
+import ta
+import backtrader as btd
+import backtesting as bts
+from backtesting import Backtest, Strategy
