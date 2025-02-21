0aebdaf0ff3fb1050760130605a2dedc6ed8c624
smarinacseas <smarinac@seas.upenn.edu>
2025-02-20 23:19:27 -0600

    Added comment for testing endpoints using local host

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 # Run the following command to start the API on local host:
 # uvicorn backend.app.main:app --reload
+# Navigate to http://127.0.0.1:8000/docs to see the API documentation
 
 load_dotenv()  # This will load variables from a .env file into the environment
 
